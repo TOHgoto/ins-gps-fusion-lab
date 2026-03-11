ca39a32e2e92d92e765bbcb72ba43934785aaa9e
Wang Weinan <TOHMail@163.com>
2026-03-12 05:37:15 +0800

    cleanup: remove legacy directory structure after migration

--- visualization/__init__.py
+++
@@ -1,9 +0,0 @@
-"""INS-GPS Fusion Lab - Visualization module.
-
-Plotting utilities for covariance, NIS, trajectories, etc.
-"""
-
-from visualization.plot_nis import plot_nis
-from visualization.plot_covariance import plot_covariance, plot_position_ellipse
-
-__all__ = ["plot_nis", "plot_covariance", "plot_position_ellipse"]
